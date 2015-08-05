17d3addd83c1fe1921dcebb4407df80bec0411b5
Brian Rayburn <bgrayburn@gmail.com>
2015-08-05 01:55:49 +0000

    moving to port 80

--- start_container.sh
+++ start_container.sh
@@ -1,4 +1,4 @@
 docker kill homepage
 docker rm homepage
-docker run -it --name=homepage --link mongodb:mongodb --rm -p 3000:3000 -v "$(pwd)":/app -e "MONGO_URL=mongodb://mongodb/meteor" danieldent/meteor meteor -p 80
+docker run -it --name=homepage --link mongodb:mongodb --rm -p 80:80 -v "$(pwd)":/app -e "MONGO_URL=mongodb://mongodb/meteor" danieldent/meteor meteor -p 80
 #docker run -it --name=homepage --link mongodb:mongodb --rm -p 3000:3000 -v "$(pwd)":/app danieldent/meteor meteor 
